0069e806b0dc686210ff5b07770aaa42b059d146
xoan.xc <61795755+xoanxc@users.noreply.github.com>
2023-12-09 14:11:48 +0100

    Create SFC-DISM.bat

---
+++ script/SFC-DISM.bat
@@ -0,0 +1,57 @@
+@echo off
+color 2
+
+echo.
+echo.
+echo              Reparación de archivos del sistema y optimización
+echo.
+echo.
+echo              Presiona cualquier tecla para continuar...
+pause >nul
+
+echo.
+echo [-] Verificando y reparando archivos del sistema (sfc /scannow)...
+echo.
+
+sfc /scannow
+if %ERRORLEVEL% neq 0 (
+    echo [!] Error durante la verificación y reparación de archivos del sistema.
+) else (
+    echo [+] Verificación y reparación completadas.
+)
+
+echo [-] Verificando la salud de la imagen del sistema (DISM /Online /Cleanup-Image /CheckHealth)...
+echo.
+
+DISM /Online /Cleanup-Image /CheckHealth
+if %ERRORLEVEL% neq 0 (
+    echo [!] Error durante la verificación de salud de la imagen del sistema.
+) else (
+    echo [+] Verificación de salud de la imagen del sistema completada.
+)
+
+echo [-] Escaneando la salud de la imagen del sistema (DISM.exe /Online /Cleanup-image /Scanhealth)...
+echo.
+
+DISM.exe /Online /Cleanup-image /Scanhealth
+if %ERRORLEVEL% neq 0 (
+    echo [!] Error durante el escaneo de salud de la imagen del sistema.
+) else (
+    echo [+] Escaneo de salud de la imagen del sistema completado.
+)
+
+echo [-] Restaurando la salud de la imagen del sistema (DISM.exe /Online /Cleanup-image /Restorehealth)...
+echo.
+
+DISM.exe /Online /Cleanup-image /Restorehealth
+if %ERRORLEVEL% neq 0 (
+    echo [!] Error durante la restauración de salud de la imagen del sistema.
+) else (
+    echo [+] Restauración de salud de la imagen del sistema completada.
+)
+
+echo.
+echo [+] Proceso de reparación y optimización completado.
+echo.
+
+pause
